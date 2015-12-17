2e65f8a7e89a41a8efdab427ff217a28b5f99a18
Brian Woodward <brian.woodward@gmail.com>
2015-12-16 21:45:59 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * unlazy-loader <https://github.com/doowb/unlazy-loader>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
