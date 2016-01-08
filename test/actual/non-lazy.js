d8f919cce8f3fa39122b1dbd4dc3dd7716a79c46
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-08 11:49:37 -0500

    unignore actual

---
+++ test/actual/non-lazy.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var path = require('path');
+
+/**
+ * Module dependencies
+ */
+
+var utils = {};
+
+
+
+utils.merge = require('mixin-deep');
+utils.through = require('through2');
+utils.File = require('vinyl');
+
+utils.foo = function() {
+
+};
+
+module.exports = utils;
---
+++ test/actual/top.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+var lazy = {};
+lazy.merge = require('mixin-deep');
+lazy.through = require('through2');
+lazy.File = require('vinyl');
+module.exports = lazy;
